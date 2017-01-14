885939dfe9e8732aa787537080d9f1e5aa79d682
jaugeni <jaugeni@gmail.com>
2017-01-14 15:54:34 -0500

    instoled Alamofire

--- PokeDex/PokemonDetailVC.swift
+++ PokeDex/PokemonDetailVC.swift
@@ -9,18 +9,36 @@
 import UIKit
 
 class PokemonDetailVC: UIViewController {
-
+    
     var pokemon: Pokemon!
     
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexIdLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var AttackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       nameLbl.text = pokemon.name
+        
+        nameLbl.text = pokemon.name.capitalized
         
     }
-
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
     
     
- 
+    
 }
